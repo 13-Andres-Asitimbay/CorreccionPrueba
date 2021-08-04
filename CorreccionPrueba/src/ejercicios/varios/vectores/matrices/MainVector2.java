ab5b3d1b5b0e1447cf735898fffe2c9f8f6bf2ad
AndrosBY <AndrosBY@AndrosPC-13>
2021-08-03 19:50:48 -0500

    a cuanto los tamales

--- CorreccionPrueba/src/ejercicios/varios/vectores/matrices/MainVector2.java
+++ CorreccionPrueba/src/ejercicios/varios/vectores/matrices/MainVector2.java
@@ -8,14 +8,13 @@ public class MainVector2 {
 /*Declare un vector X cuya longitud se solicite al usuario de tipo int, y finalmente se
 impriman los valores asignados*/
 		System.out.print("Ingrese la dimension del vector: ");
-		Scanner teclado1 = new Scanner(System.in);
-		int Z = teclado1.nextInt();
+		Scanner teclado = new Scanner(System.in);
+		int Z = teclado.nextInt();
 		int X[] = new int[Z];
 		System.out.println(" ");
 		
 		for(int i = 0 ; i < Z ; i++) {
 			System.out.print("Ingrese un valor para la posicion " + i + ": ");
-			Scanner teclado = new Scanner(System.in);
 			int N = teclado.nextInt();
 			X[i] = N;
 		}
